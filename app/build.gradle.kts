706a30d173418414ccb8c373031dcc6fd730b6f1
Md. Zahid Alam <74257049+Md-Zahid-Alam@users.noreply.github.com>
2026-08-06 19:21:20 +0600

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.myemitracker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "3.0"
+        versionCode = 5
+        versionName = "4.1"
     }
 
     compileOptions {
