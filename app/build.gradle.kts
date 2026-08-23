3f6956eeba062ae43be8cbf540bde709c9c837f2
Md. Zahid Alam <74257049+Md-Zahid-Alam@users.noreply.github.com>
2026-08-23 15:09:03 +0600

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mdzahidalam.myfinancetracker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 19
-        versionName = "6.0"
+        versionCode = 20
+        versionName = "6.1"
     }
 
     val stableStorePath = System.getenv("MFT_SIGNING_STORE_FILE")
