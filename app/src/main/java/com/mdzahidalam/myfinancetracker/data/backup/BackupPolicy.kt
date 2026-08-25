20eb46f4f1c8be08f68b4e5376a475833d56c9c3
Md. Zahid Alam <74257049+Md-Zahid-Alam@users.noreply.github.com>
2026-08-25 18:02:51 +0600

    Delete app directory

--- app/src/main/java/com/mdzahidalam/myfinancetracker/data/backup/BackupPolicy.kt
+++
@@ -1,8 +0,0 @@
-package com.mdzahidalam.myfinancetracker.data.backup
-
-object BackupPolicy {
-    const val MAX_PAYLOAD_LENGTH = 40_000_000
-    const val DEFAULT_ITERATIONS = 210_000
-    fun validatedIterations(value: Int) = value.coerceIn(120_000, 500_000)
-    fun requireAcceptableSize(length: Int) = require(length <= MAX_PAYLOAD_LENGTH) { "Backup file is too large." }
-}
